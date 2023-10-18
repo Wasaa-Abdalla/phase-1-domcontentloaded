e6554f19c13d580a817a841fe6b078be5a9764d7
abdalla-wasaa <abdallawasaa7@gmail.com>
2023-10-18 15:11:28 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const newText = document.getElementById("text");
+    newText.textContent = "This is really cool!";
+});
